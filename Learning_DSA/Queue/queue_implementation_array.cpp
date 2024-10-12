50755c083441b11b8b715cfa1b31384234e8f1b2
Ankush Kumar <floatankush@gmail.com>
2024-10-13 03:24:39 +0530

    queue implementation using array

--- Learning_DSA/Queue/queue_implementation_array.cpp
+++ Learning_DSA/Queue/queue_implementation_array.cpp
@@ -79,6 +79,16 @@ public:
         }
         cout << endl;
     }
+    // Function to print the first element of the queue
+    void printFront()
+    {
+        if (isEmpty())
+        {
+            cout << "Queue is empty. No front element." << endl;
+            return;
+        }
+        cout << "Front element is: " << arr[front] << endl;
+    }
 };
 
 int main()
@@ -99,7 +109,18 @@ int main()
     q.enqueue(60); // This will show queue is full
 
     q.dequeue();
+    q.dequeue();
     q.display();
+    q.printFront();
 
     return 0;
 }
+
+//  problem with the above
+/*
+Wasted Space (Inefficient Memory Usage):
+Simple Queue: Once the front pointer moves past the first
+element due to dequeuing, the memory space used by dequeued
+elements is effectively wasted and cannot be reused. For example,
+if you enqueue 5 elements and dequeue 3, the remaining space at the
+front of the array cannot be reused even if the queue has space left overall. */
