a49018cfc26e72059e7476cb84891c17d6180565
Ankush Kumar <floatankush@gmail.com>
2025-03-23 22:25:43 +0530

    print even number in reverse order

---
+++ Learning_DSA/Recursion/printEven_reverse.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+
+void printEven(int n)
+{
+    if(n==2){
+        cout<<n<<" ";
+        return ;
+    }
+    cout<< n << " ";
+    printEven(n-2);
+}
+int main(){
+    int n;
+    cout<<"Enter Number : ";
+    cin>>n;
+    if(n%2==0){
+    printEven(n);
+
+    };
+    if(n%2!=0){
+        n=n-1;
+        printEven(n);
+    }
+}
