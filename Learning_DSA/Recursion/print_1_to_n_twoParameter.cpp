91d51e271c972b89687457b33c4908629956ebbb
Ankush Kumar <floatankush@gmail.com>
2025-03-23 22:10:00 +0530

    printing 1 to by passing two parameter

---
+++ Learning_DSA/Recursion/print_1_to_n_twoParameter.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void printNumbers(int start, int end)
+{
+    if (start > end)
+        return;
+    cout << start << " ";
+    printNumbers(start + 1, end);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+    printNumbers(1, n);
+    return 0;
+}
