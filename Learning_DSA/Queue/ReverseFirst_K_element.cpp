2b3d589e14cbcd71eb738d08f9fac80bcf80f1a1
Ankush Kumar <floatankush@gmail.com>
2025-03-29 23:37:33 +0530

    Reverse first k element in array

---
+++ Learning_DSA/Queue/ReverseFirst_K_element.cpp
@@ -0,0 +1,50 @@
+// Reversing the first k element of the queue
+#include<iostream>
+#include<stack>
+#include<queue>
+using namespace std;
+
+void ReverseFirstK(queue<int> &q){
+    int k;
+    cout<<"Enter the value of K : "; cin>>k;cout<<endl;
+
+    stack<int>s;
+    while(k--){
+        s.push(q.front());
+        q.pop();
+    }
+    int size=q.size();
+    while(!s.empty()){
+        q.push(s.top());
+        s.pop();
+    }
+    while(size--){
+        q.push(q.front());
+        q.pop();
+    }
+    
+}
+void display(queue<int> q){
+    while(!q.empty()){
+        cout<<q.front()<<" ";
+        q.pop();
+    }
+    cout<<endl;
+}
+
+int main(){
+    queue<int>q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    q.push(5);
+    q.push(6);
+    q.push(7);
+
+    cout<<"Queue : Original queue: ";
+    display(q);
+    ReverseFirstK(q);
+    cout<<"Queue: k element reversal "; display(q);
+
+}
