199bc695bb809fd117e43fea8e5b440b28607204
Ankush Kumar <floatankush@gmail.com>
2025-04-11 23:35:48 +0530

    insertion at beginning of the array

---
+++ Learning_DSA/Array/Basic_doubt_in_array/array_declaration_initialization.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Correct declaration and initialization
+    int arr1[5] = {1, 2, 3, 4, 5};
+
+    // Partial initialization: rest become 0
+    int arr2[5] = {10, 20};
+
+    // Size inferred automatically
+    int arr3[] = {7, 8, 9};
+
+    cout << "arr2[2]: " << arr2[2] << endl;  // Outputs 0
+}
