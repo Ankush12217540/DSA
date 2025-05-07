e50fa09352160cda162587a082d29f423ed79eed
Ankush Kumar <floatankush@gmail.com>
2025-05-07 23:57:01 +0530

    revised insertion sort

--- Learning_DSA/Array/insertion_sort.cpp
+++ Learning_DSA/Array/insertion_sort.cpp
@@ -15,11 +15,14 @@ void insertionSort(int arr[], int n) {
     }
 }
 
+
 void printArray(int arr[], int n) {
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
     cout << endl;
 }
+
+
 
 int main() {
     int arr[] = {12, 11, 13, 5, 6};
