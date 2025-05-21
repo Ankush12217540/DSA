db4ec3fcd40594dd95b2b425be3e206b06d0d91a
Ankush Kumar <floatankush@gmail.com>
2025-05-21 23:50:46 +0530

    revised

--- Learning_DSA/Recursion/minimum_element.cpp
+++ Learning_DSA/Recursion/minimum_element.cpp
@@ -8,8 +8,10 @@ int minElement(int arr[],int start,int end){
     int min=arr[start]; if(arr[start] > arr[start+1]) min=arr[start+1];
     if(arr[start]< arr[start+1]) min = arr[start];
     if(start==end) return min;
+    
     return minElement(arr,start+1,end);
 }
+
 
 int main(){
     int arr[]={1,3,4,6,7,6,4,0, -1,5} , start =0 , end=7;
